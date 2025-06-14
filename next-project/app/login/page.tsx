b7cba84467df9a902a889d0c2833fb29b5048a79
OmAmberkar <omamberkar08@gmail.com>
2025-06-14 18:17:30 +0530

    feat: implement login and registration pages with authentication handling

---
+++ next-project/app/login/page.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import { signIn } from 'next-auth/react'
+import { error } from 'console';
+
+function Loginpage() {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const router = useRouter();
+
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        const result = await signIn("credentials",{
+            email,
+            password,
+            redirect:false
+        })
+
+        if(result?.error){
+            console.log(result.error)
+        }else{
+            router.push('/')
+        }
+    }  ;
+    
+    return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="email"
+            placeholder='Email'
+            value={email}
+            onChange={ (e) => setEmail(e.target.value)} />
+            <input type="password"
+            placeholder='Password'
+            value={password}
+            onChange={ (e) => setPassword(e.target.value)} />
+            <button type='submit'>Login</button>
+      </form>
+      <div>
+        Don&apos;t have an account ?? <a href="/register">Register</a>
+      </div>
+    </div>
+  )
+}
+
+export default Loginpage
