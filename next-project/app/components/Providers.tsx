cb8c77791289b4492507a7852132a8c8548f259c
OmAmberkar <omamberkar08@gmail.com>
2025-06-15 13:07:24 +0530

    updates regarding the fileupload and setting the page to upload the reel from local storage

--- next-project/app/components/Providers.tsx
+++ next-project/app/components/Providers.tsx
@@ -1,16 +1,13 @@
 "use client";
-import { ImageKitProvider } from "@imagekit/next"
-import { SessionProvider } from "next-auth/react"
+import { ImageKitProvider } from "@imagekit/next";
+import { SessionProvider } from "next-auth/react";
 
-const urlEndPoint = process.env.NEXT_PUBLIC_PUBLIC_KEY!;
-if(!urlEndPoint){
-    throw new Error("Public Key is not defined");
-}
+const urlEndPoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
 
-export default function Providers({children}: {children: React.ReactNode}) {
-    return <SessionProvider refetchInterval={5*60}>
-        <ImageKitProvider urlEndpoint={urlEndPoint}>
-    {children}
-    </ImageKitProvider>
-    </SessionProvider>;
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider refetchInterval={5 * 60}>
+      <ImageKitProvider urlEndpoint={urlEndPoint}>{children}</ImageKitProvider>
+    </SessionProvider>
+  );
 }
--- next-project/app/login/page.tsx
+++ next-project/app/login/page.tsx
@@ -1,49 +1,51 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
-import { signIn } from 'next-auth/react'
-import { error } from 'console';
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
-function Loginpage() {
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const router = useRouter();
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
 
-    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) =>{
-        e.preventDefault();
-        const result = await signIn("credentials",{
-            email,
-            password,
-            redirect:false
-        })
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        if(result?.error){
-            console.log(result.error)
-        }else{
-            router.push('/')
-        }
-    }  ;
-    
-    return (
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      console.log(result.error);
+    } else {
+      router.push("/");
+    }
+  };
+
+  return (
     <div>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <input type="email"
-            placeholder='Email'
-            value={email}
-            onChange={ (e) => setEmail(e.target.value)} />
-            <input type="password"
-            placeholder='Password'
-            value={password}
-            onChange={ (e) => setPassword(e.target.value)} />
-            <button type='submit'>Login</button>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
       </form>
       <div>
-        Don&apos;t have an account ?? <a href="/register">Register</a>
+        Don&apos;t have an account ?
+        <button onClick={() => router.push("/register")}>Register</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Loginpage
+export default LoginPage;
